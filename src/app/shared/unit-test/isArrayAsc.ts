789864ddfe114df155492783b75c4eb5c950b3eb
Ha Hoang T <ha.hoang@supremetech.vn>
2023-04-23 22:02:56 +0700

    Check array is asc

---
+++ src/app/shared/unit-test/isArrayAsc.ts
@@ -0,0 +1,7 @@
+export const isArrayAsc = (arr: number[]) => {
+  if (!Array.isArray(arr) || arr.length < 2) {
+    return false;
+  }
+  return arr.every((item: number, i: number) => typeof item === 'number'
+    && (i === 0 || item >= arr[i - 1]));
+};
